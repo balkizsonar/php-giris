038540d7e6a1827332ea9a282e0b5fccda7f7b9a
Ozan SONAR <114029075+balkizsonar@users.noreply.github.com>
2023-02-03 14:56:06 +0300

    cookie

---
+++ session/ornek-1/cookie.php
@@ -0,0 +1,20 @@
+<?php
+session_start();//bundan önce hiçbir şey yazamazsın
+
+/*
+$_SESSION['uye'] = [ // uye içindeki kullanıcı adını dizi olarak oluşturduk
+    'kullanici_adi' => 'balkız',
+    'sifre' => '123'
+];
+*/
+
+//print_r($_SESSION);
+
+
+
+//setcookie('uye[id]', 1, time() + 86400);
+//setcookie('uye[kullanici_adi]', 'tayfun', time() + 86400);
+//setcookie('uye[sifre]', 123, time() + 86400)
+
+print_r($_COOKIE);
+?>
